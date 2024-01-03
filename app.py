439e919032a52aab5059a841b1576666a0969459
harry0200 <114043478+harry0200@users.noreply.github.com>
2024-01-03 18:27:42 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -10,15 +10,15 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import *
-
+import re
 app = Flask(__name__)
 
 # 必須放上自己的Channel Access Token
-line_bot_api = LineBotApi('6BHlbOR673DiFlcS3JhClIBBZ3QmB8UT2skmk00loO2pzdi+BMVVCnDqLe7GaOEbqVFtoAd1ZU5AW+BhiTqlwI5U5QJzGS2LuBd3vWuMQrf1MHE86jWf1075E+EM47uwlziD7JnYDkn1pQ617axphwdB04t89/1O/w1cDnyilFU=')
+line_bot_api = LineBotApi('JpVMfpzpD+Stu3nfpfB8bWKcsmkCqasZa5rPaYgaQzGP17NqllQM6emvpBm5vHCIyag9ZcC2TIP9sqdUr0ZAMGJNuV/ccL21b79pgTdg7oEQuWzrj4rEkc78jASuSpAZT74uBKzN7ZDvKGInWTVXAgdB04t89/1O/w1cDnyilFU=')
 # 必須放上自己的Channel Secret
-handler = WebhookHandler('2e0a240a4422579dde659987460cccc7')
+handler = WebhookHandler('ca5327b5c7ba783d20a3c85738bfd1a4')
 
-line_bot_api.push_message('U86cbd03e54cd463a7059c65e5220c1a7', TextSendMessage(text='你可以開始了'))
+line_bot_api.push_message('U262565b00a73c456ebba11b0bd1e7762', TextSendMessage(text='你可以開始了'))
 
 # 監聽所有來自 /callback 的 Post Request
 @app.route("/callback", methods=['POST'])
@@ -42,9 +42,36 @@ def callback():
 ##### 基本上程式編輯都在這個function #####
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
-    line_bot_api.reply_message(event.reply_token,message)
-
+    message = text=event.message.text
+    if re.match('更多資訊',message):
+        buttons_template_message = TemplateSendMessage(
+        alt_text='這是樣板傳送訊息',
+        template=ButtonsTemplate(
+            thumbnail_image_url='https://imgur.com/bY07QAW',
+            title='寵物用品公司',
+            text='選單功能',
+            actions=[
+                PostbackAction(
+                    label='官方網站',
+                    uri='https://chongwuyongpinzhuanmai.webnode.tw/'
+                ),
+                MessageAction(
+                    label='購買狗狗商品',
+                    uri='https://chongwuyongpinzhuanmai.webnode.tw/%e7%8b%97%e7%8b%97%e7%94%a2%e5%93%81/'
+                ),
+                URIAction(
+                    label='購買貓咪商品',
+                    uri='https://chongwuyongpinzhuanmai.webnode.tw/%e8%b2%93%e5%92%aa%e7%94%a2%e5%93%81/'
+                )
+                URIAction(
+                    label='尚未新增'
+                )
+            ]
+        )
+    )
+        line_bot_api.reply_message(event.reply_token, buttons_template_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
 #主程式
 import os
 if __name__ == "__main__":
